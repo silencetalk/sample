325cb698b21397c58d25a86441879c70e6c1599a
vagrant <vagrant@homestead>
2018-09-16 15:40:07 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,9 +2,35 @@
 <html>
 <head>
     <title>@yield('title','Sample')- Laravel 新手入门教程</title>
+    <link rel="stylesheet"  href="/css/app.css">
 </head>
 <body>
-    @yield('content')
+    <!--
+    <header class="nvbar nvbar-fixed-top navbar-inverse">
+        <div class="container">
+            <div class="col-md-offset-1 col-md-10">
+                <a href="/" id="logo">Sample App</a>
+                <nav>
+                    <ul class="nav nvbar-nav navbar-right">
+                        <li><a href="/help">帮助</a></li>
+                        <li><a href="#">登录</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
 
+
+    </header>
+    !-->
+    @include('layouts._header')
+
+    <div class="container">
+      <div class="col-md-offset-1 col-md-10">
+        @yield('content')
+        @include('layouts._footer')
+      </div>
+    </div>
+
+   
 </body>
 </html>
---
+++ resources/views/users/create.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.defealt')
+@section('title','注册')
+
+@section('content')
+<h1>注册</h1>
+@stop
