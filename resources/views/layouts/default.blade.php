437da02f7ca3166cebfc165a5f981faa4fcea2d9
vagrant <vagrant@homestead>
2018-09-25 14:13:38 +0000

    Finish Login and Logout lalala

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,23 +5,8 @@
     <link rel="stylesheet"  href="/css/app.css">
 </head>
 <body>
-    <!--
-    <header class="nvbar nvbar-fixed-top navbar-inverse">
-        <div class="container">
-            <div class="col-md-offset-1 col-md-10">
-                <a href="/" id="logo">Sample App</a>
-                <nav>
-                    <ul class="nav nvbar-nav navbar-right">
-                        <li><a href="/help">帮助</a></li>
-                        <li><a href="#">登录</a></li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
 
 
-    </header>
-    !-->
     @include('layouts._header')
 
     <div class="container">
@@ -32,6 +17,6 @@
       </div>
     </div>
 
-   
+   <script src="/js/app.js"></script>
 </body>
 </html>
--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -6,7 +6,7 @@
     <div class="col-md-12">
       <div class="col-md-offset-2 col-md-8">
         <section class="user_info">
-          @include('shared._user_info', ['user' => $user])
+          @include('shared._user_info', array('user' => $user))
         </section>
       </div>
     </div>
