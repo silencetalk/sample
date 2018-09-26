e61e190077ea73474fa4026f7b4058cfba95ff4a
vagrant <vagrant@homestead>
2018-09-26 15:24:04 +0000

    Finish user edit,update,index,and destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -29,7 +29,7 @@ class SessionsController extends Controller
             session()->flash('danger', '很抱歉，您的邮箱和密码不匹配');
             return redirect()->back();
         }
-        return;
+        //return;
     }
 
     public function destroy()
@@ -37,5 +37,11 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash('success','您已成功退出！');
         return redirect('login');
+    }
+    public function __construct()
+    {
+        $this->middleware('guest',[
+            'only' => ['create']
+        ]);
     }
 }
